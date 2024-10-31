807147652a72e48688e483bd474904c1f5d0a30a
BiMalxMe <journeyxbimal@gmail.com>
2024-10-31 14:02:43 +0545

    Frontent dailycode

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -13,24 +13,27 @@ export const BlogCard=({
     content,
     publishedDate
 }:BlogcardType)=>{
-    return (<div>
-        <div>
-          {<Avatar name={authorName}/>}  {authorName}. {publishedDate}
+    return (<div className="pb-4  px-7">
+        <div className="font-extralight">
+          {<Avatar name={authorName} />}  <span className="font-medium mr-4">{authorName}.</span> {publishedDate}
         </div>
-        <div>
+        <div className="text-xl font-semibold">
             {title}
         </div>
    
-        <div>
+        <div className=" text-md font-thin">
             {content}
+        </div>
+        <div className="text-slate-400 pt-3">
+            {`${Math.ceil((content.length)/100)} Minute read`}
         </div>
     </div>)
 }
 
-function Avatar({name}:{name:string}){
-    return (<div>
-        <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-    <span className="font-medium text-gray-600 dark:text-gray-300">{name[0].toUpperCase()}</span>
-</div>
-    </div>)
+export function Avatar({name}:{name:string}){
+    return (
+        <div className="mr-1 relative inline-flex items-center justify-center w-7 h-7 overflow-hidden bg-gray-300 rounded-full dark:bg-gray-600">
+    <span className=" font-medium text-gray-900 dark:text-gray-300">{name[0].toUpperCase()}</span>
+        </div>
+        )
 }
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,13 +1,27 @@
+import { Appbar } from "../components/Appbar"
 import { BlogCard } from "../components/BlogCard"
+import { useBlogs } from "../components/hooks"
 
 
 
 export const Blogs=()=>{
-    return <div className="shadow-md">
+    const {loading,blogs}=useBlogs()
+    if(loading){
+        return <div>
+            Loading........
+        </div>
+    }
+    return <div>
+    <Appbar />
+    <div className="flex justify-center">
+    <div className="border-b border-slate-300  max-w-xl">
        <BlogCard     
     authorName={"Bimal"}
     title={"the todays Generation"}
-    content={"It is based  on the first century of the world development"}
+    content={"It is based  on the first asdgjnbaikreiorefjhgierhgiewrhgirewjhigrhgirehgikcentury of the world development"}
     publishedDate={"2nd Feb 2022"}/>
+      
+    </div>
+    </div>
     </div>
 }
