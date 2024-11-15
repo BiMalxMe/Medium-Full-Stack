7e825dea4eb9daca9dd14934f8b1c71bc7587848
BiMalxMe <journeyxbimal@gmail.com>
2024-11-15 20:19:39 +0545

    'final'

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -4,23 +4,41 @@ import { FullBlog } from "../components/FullBlog";
 import { BlogSkeleton } from "../components/BlogSkeleton";
 import { Appbar } from "../components/Appbar";
 
+
 export const Blog = () => {
   const { id } = useParams();
   const { loading, blog } = useBlog({ id: id || "" });
   if (loading) {
-    return <div>
-      <Appbar />
-    <div className="flex justify-center">
+    // If data is still loading, show the skeleton loaders
+    return (
       <div>
-        <BlogSkeleton />
-        <BlogSkeleton />
-        <BlogSkeleton />
-        <BlogSkeleton />
+        <Appbar />
+        <div className="flex justify-center">
+          <div>
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+          </div>
         </div>
+      </div>
+    );
+  } else if (blog) {
+    // If the blog data is available, render FullBlog
+    return (
+      <div>
+        <FullBlog blog={blog} />
+      </div>
+    );
+  } else {
+    // If there's an error (i.e., blog is not available)
+    return (
+      <div>
+        <Appbar />
+        <div className="flex justify-center">
+          <div>An error occurred. Please try again later.</div>
         </div>
-        </div>;
+      </div>
+    );
   }
-  return <div>
-        <FullBlog blog={blog}/>
-  </div>
-};
+}  
